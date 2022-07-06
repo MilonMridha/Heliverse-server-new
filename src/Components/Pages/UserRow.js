1366d1e919bb4bee99578aa2cc65c4f689bef9fb
MilonMridha <milonmridha570@gmail.com>
2022-07-07 05:47:09 +0600

    add delete api

--- src/Components/Pages/UserRow.js
+++ src/Components/Pages/UserRow.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const UserRow = ({ user, refetch, index }) => {
     const { name, email, _id } = user;
     const navigate = useNavigate();
     const handleDetails = (id) => {
         navigate('/user/' + id)
+    }
+    const handleDelete = id =>{
+        const url = `http://localhost:5000/users/${id}`;
+
+                fetch(url, {
+                    method: 'DELETE'
+                }).then(res => res.json())
+                .then(data => {
+                    if(data.acknowledged === true){
+                        toast.error('User Deleted Successfully')
+                        }
+                        refetch()
+                       
+                })
     }
 
     return (
@@ -16,9 +31,9 @@ const UserRow = ({ user, refetch, index }) => {
             <td>{_id}</td>
             <td><button onClick={() => handleDetails(_id)} className="btn btn-xs rounded-full text-white p-2 bg-black">Details</button>
             </td>
-            <td><button className="btn btn-xs rounded-full text-white p-2 bg-green-700">Update</button>
+            <td><button   className="btn btn-xs rounded-full text-white p-2 bg-green-700">Update</button>
             </td>
-            <td><button className="btn btn-xs rounded-full text-white p-2 bg-red-700">Delete</button>
+            <td><button onClick={() => handleDelete(_id)}  className="btn btn-xs rounded-full text-white p-2 bg-red-700">Delete</button>
             </td>
 
         </tr>
